bfbffe41a8a8d7681d8bea6bbe83300cba92103d
daryapatsko <darya.patsko@mail.ru>
2023-08-15 22:20:29 +0300

    init:add hw38

---
+++ my-app/src/components/Post/Post.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import "./style.css"
+
+
+
+interface IPost{
+  "id": number,
+  "image": string,
+  "text": string,
+  "date": string,
+  "lesson_num"?: number,
+  "title": string,
+  "description"?: string,
+  "author"?: number
+}
+
+const Post = ({id, image,text,date,title,description}:IPost) => {
+  return (
+    <div className='post__box' key={id}>
+      <div className="content__post-box">
+        <h3 className='date__post'>{date}</h3>
+        <h3 className='title__post'>{title}</h3>
+        <p className="description__post">{description}</p>
+      </div>
+      <div className="image__post"><img src={image} alt={text} /></div>
+
+
+      
+    </div>
+  )
+}
+
+export default Post
